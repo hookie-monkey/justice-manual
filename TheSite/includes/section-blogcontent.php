b01ef777e271ec97dc9f4fc926b2d940753ffa3e
hookie-monkey <46463514+hookie-monkey@users.noreply.github.com>
2020-05-20 18:48:54 -0500

    new single header

---
+++ TheSite/includes/section-blogcontent.php
@@ -0,0 +1,31 @@
+<?php if ( have_posts() ): while ( have_posts() ): the_post();?>
+
+<p><?php echo get_the_date();?></p>
+
+<?php the_content();?>
+
+<?php 
+ $fname = get_the_author_meta ('first_name'); ?>
+ <p> Posted by <?php echo $fname;?> </p>
+ 
+<?php $tags = get_the_tags();
+foreach($tags as $tag):?>
+
+
+    <a href="<?php echo get_tag_link($tag->term_id);?> " class="badge badge-success">
+        <?php echo $tag->name;?>
+    </a>
+
+<?php endforeach;?>
+
+<?php
+$categories = get_the_category();
+foreach($categories as $cat):?>
+
+<p><a href="<?php echo get_category_link($cat-> term_id);?>">
+
+    <?php echo $cat->name;?></a>
+</p>
+<?php endforeach; ?>
+
+    <?php endwhile; else: endif;?>
